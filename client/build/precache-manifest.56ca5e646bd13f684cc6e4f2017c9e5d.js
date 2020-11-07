cda1a39e79140d43ff4856e28a1e8956a7389250
kashi11 <68692244+kashi11@users.noreply.github.com>
2020-11-07 23:29:45 +0500

    Add files via upload

---
+++ client/build/precache-manifest.56ca5e646bd13f684cc6e4f2017c9e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5c3cde91c47b15a7f708ba1d9c69be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ab74478521587d050d0",
+    "url": "/static/css/main.6b9838d7.chunk.css"
+  },
+  {
+    "revision": "e8ae790740e98c49028f",
+    "url": "/static/js/2.0ea80727.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0ea80727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab74478521587d050d0",
+    "url": "/static/js/main.7bf60da1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
